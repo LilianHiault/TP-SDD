8dd0d386be5a6a3e9c12a1c7478fa88cdf8c6829
Lilian Hiault <lilian.hiault@laposte.net>
2021-02-10 14:59:34 +0100

    Update main.c : coutFaibles

--- main.c
+++ main.c
@@ -4,6 +4,51 @@
 #include "matrice.h"
 #include <stdio.h>
 
+
+cellule_t * coutFaibles(int k, int ** C, int m, int n)
+/* coutFaibles : crée la liste des K plus petites valeurs de la matrice C
+   Entrée : k valeurs à mettre dans la liste
+            C la matrice en 2 dimensions de taille m * n
+	    m lignes dans C
+	    n colonnes dans C
+   Sortie :
+   adresse vers la première cellule */
+{
+  cellule_t * liste = NULL;
+  int i, j;
+  // On rempli d'abord la liste de K valeurs
+  // La liste est triée dans l'ordre décroissant, on a donc la plus grande valeur dans le 1er maillon
+  for(i=0; i<m; i++)
+    {
+      for(j=0; j<n; j++)
+	{
+	  cellule_t c = creerCell(C[i][j], i, j);
+	  insererCell(*liste, &c);
+	}
+    }
+
+  // On parcours chaque case de la matrice
+  for(i; i<m; i++)
+    {
+      for(j; j<n; j++)
+	{
+	  // Si la valeur est plus petite que la valeur du 1er maillon (plus grande valeur de la liste)
+	  if(C[i][j] < valeurTete(liste))
+	    {
+	      // Alors on supprime le 1er maillon et on insére dans la liste triée la nouvelle valeur
+	      supprTete(liste);
+	      cellule_t c = creerCell(C[i][j], i, j);
+	      insererCell(*liste, &c);
+	    }
+	}
+    }
+  libereMatrice(C, m);
+  return liste;
+}
+  
+  
+
+
 int main(int argc, char * argv[])
 {
   int n, m;
