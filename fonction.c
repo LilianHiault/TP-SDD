14d167e263a8070f95a000d3e450e435496439a2
Lilian Hiault <lilian.hiault@laposte.net>
2021-02-24 15:31:20 +0100

    Correction parcours complet de la matrice

--- fonction.c
+++ fonction.c
@@ -13,11 +13,11 @@ cellule_t * coutFaibles(int k, int ** C, int m, int n)
    adresse vers la première cellule */
 {
   cellule_t * liste = NULL;
-  int i = 0;
-  int j = 0;
-  int val = 0;
-  int erreur = 0;
+  int i = 0; // usines
+  int j = 0; // périodes
+  int val = 0; // Valeur à insérer dans la liste
   int compt = 0;
+  cellule_t ** prec = NULL;
   // On rempli d'abord la liste de K valeurs
   // La liste est triée dans l'ordre décroissant, on a donc la plus grande valeur dans le 1er maillon
   while(i < m && compt < k)
@@ -27,11 +27,17 @@ cellule_t * coutFaibles(int k, int ** C, int m, int n)
 	  cellule_t * c = nouvCellule(C[i][j], i, j);
 	  if(c)
 	    {
-	      adjCellule(&liste, c);
+	      if(liste != NULL)
+		{
+		  val = valeur(liste);
+		}
+	      prec = rechercheTri(&liste, val);
+	      adjCellule(prec, c);
 	    }
 	  j++;
 	  compt++;
 	}
+      j = 0;
       i++;
     }
 
@@ -42,17 +48,15 @@ cellule_t * coutFaibles(int k, int ** C, int m, int n)
 	{
 	  // Si la valeur est plus petite que la valeur du 1er maillon (plus grande valeur de la liste)
 	  val = valeur(liste);
-	  if(!erreur)
+	  if(C[i][j] < val)
 	    {
-	      if(C[i][j] < val)
+	      // Alors on supprime le 1er maillon et on insére dans la liste triée la nouvelle valeur
+	      supprCellule(liste);
+	      cellule_t * c = nouvCellule(C[i][j], i, j);
+	      if(c)
 		{
-		  // Alors on supprime le 1er maillon et on insére dans la liste triée la nouvelle valeur
-		  supprCellule(liste);
-		  cellule_t * c = nouvCellule(C[i][j], i, j);
-		  if(c)
-		    {
-		      adjCellule(&liste, c);
-		    }
+		  prec = rechercheTri(&liste, valeur(liste));
+		  adjCellule(&liste, c);
 		}
 	    }
 	}
@@ -63,7 +67,7 @@ cellule_t * coutFaibles(int k, int ** C, int m, int n)
 void suppUsine(cellule_t ** a0, int u)
 /* suppUsine : supprime toutes les occurences d'une usine dans une liste
    Entrée : cellule_t * a0 l'adresse de la première cellule
-            int u l'usine à supprimer
+   int u l'usine à supprimer
    Sortie : */
 {
   cellule_t ** prec = a0;
@@ -82,7 +86,7 @@ void suppUsine(cellule_t ** a0, int u)
 void listToFile(FILE * fichier, cellule_t * liste)
 /* listToFile : écrit la liste dans un fichier de la manière suivante par ligne : valeur usine periode
    Entrée : fichier le fichier ou l'on veut écrire
-            liste la liste que l'on souhaite écrire
+   liste la liste que l'on souhaite écrire
    Sortie : */
 {
   cellule_t * cour = liste;
