3b6b30eaf37c67d3d983f93e8cf57794841a03cc
Lilian Hiault <lilian.hiault@laposte.net>
2021-02-28 19:21:40 +0100

    main.c : interaction en ligne de commande

--- main.c
+++ main.c
@@ -8,38 +8,54 @@
 
 int main(int argc, char * argv[])
 {
+  /* argv[1] : fichier à ouvrir */
+  /* argv[2] : fichier où écrire */
+  /* argv[3] : k nombre d'éléments à chercher */
   int n, m;
   int ** mat = NULL;
-  if(argc > 1)
+  char * nomOuv = argv[1];
+  char * nomEcr = argv[2];
+  int k = atoi(argv[3]);
+  if(argc > 3)
     {
-      FILE * fichier = fopen(argv[1], "r");
-      if (fichier)
+      FILE * fichierOuv = fopen(nomOuv, "r");
+      if (fichierOuv)
 	{
-	  mat = initMatrice(fichier, &m, &n);
+	  mat = initMatrice(fichierOuv, &m, &n);
 	  printf("Matrice : \n");
 	  afficherMatrice(mat, m, n);
-	  cellule_t * liste = coutFaibles(6, mat, m, n);
-	  printf("Liste :\n");
-	  afficherListe(liste);
+	  cellule_t * liste = coutFaibles(k, mat, m, n);
+	  /* printf("Liste :\n"); */
+	  /* afficherListe(liste); */
 	  
 	  libereMatrice(mat, m);
 	  suppUsine(&liste, 2);
 	  suppUsine(&liste, 2);
-	  printf("Liste :\n");
-	  afficherListe(liste);
+	  /* printf("Liste :\n"); */
+	  /* afficherListe(liste); */
 
-	  printf("Fichier :\n");
-	  listToFile(stdout, liste);
-	  libereListe(liste);
+	  /* printf("Fichier :\n"); */
+	  FILE * fichierEcr = fopen(nomEcr, "w");
+	  {
+	    if (fichierEcr)
+	      {
+		listToFile(fichierEcr, liste);
+		libereListe(liste);
+	      }
+	    else
+	      {
+		fprintf(stderr, "Erreur d'ouverture du fichier d'écriture\n");
+	      }
+	  }
 	}
       else
 	{
-	  fprintf(stderr, "Erreur d'ouverture du fichier\n");
+	  fprintf(stderr, "Erreur d'ouverture du fichier à lire\n");
 	}
     }
   else
     {
-      fprintf(stderr, "Aucun fichier spécifié en ligne de commande\n");
+      fprintf(stderr, "Nombre d'arguments insuffisant. Utilisez :\nprog fichier_a_ouvrir fichier_ou_ecrire nombre_elements\n");
     }
   return 0;
 }
